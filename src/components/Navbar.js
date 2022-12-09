cc64f5ad552f411f98cfd97ec1a057fb74ac2f86
ignacio gaston odella <108488609+Nano92OB@users.noreply.github.com>
2022-12-08 21:27:39 -0300

    login button & css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,31 +1,54 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const Navbar = ({change,stateChange,search}) =>{
-    return (
-        <nav> 
-            <div className="title margins">
-                 <div className="logo">
-                    <img className="imageLogo" src="/Sprites/Icons/Pokeball.png" alt="pokeball"/>
-                 
-                 <span className="textTitle margins">
-                    Pokédex
-                 </span> </div> 
-                 <div className="typeOrder" onClick={() => change()}>
-                 {(!stateChange)?
-                    <>
-                        #<img src="/Sprites/Icons/Arrow.svg" alt="arrow" className="arrowNav"/>
-                    </>
-                 :
-                    <>
-                        A<img src="/Sprites/Icons/Arrow.svg" alt="arrow" className="arrowNav"/>
-                    </>
-                }
-                </div>
-            </div>
-                  <div className="searchBar">
-                  <input type="text" placeholder="&#xF002; Buscar" className="fontAw" onChange={(e)=> search(e.target.value)}/>
-                  </div>
-        </nav>
-    )
-}
-export default Navbar
+const Navbar = ({ change, stateChange, search }) => {
+  return (
+    <nav>
+      <div className="title margins">
+        <div className="logo">
+          <img
+            className="imageLogo"
+            src="/Sprites/Icons/Pokeball.png"
+            alt="pokeball"
+          />
+          <span className="textTitle margins">Pokédex</span>{" "}
+        </div>
+        <div className="typeOrder" onClick={() => change()}>
+          {!stateChange ? (
+            <>
+              #
+              <img
+                src="/Sprites/Icons/Arrow.svg"
+                alt="arrow"
+                className="arrowNav"
+              />
+            </>
+          ) : (
+            <>
+              A
+              <img
+                src="/Sprites/Icons/Arrow.svg"
+                alt="arrow"
+                className="arrowNav"
+              />
+            </>
+          )}
+        </div>
+        <Link to="/loginform">
+          <button className="w-10 btn btn-sm btn-dark" type="submit">
+            Login
+          </button>
+        </Link>
+      </div>
+      <div className="searchBar">
+        <input
+          type="text"
+          placeholder="&#xF002; Buscar"
+          className="fontAw"
+          onChange={(e) => search(e.target.value)}
+        />
+      </div>
+    </nav>
+  );
+};
+export default Navbar;
